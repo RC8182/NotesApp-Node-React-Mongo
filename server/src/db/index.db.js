879a3f8e33a3a26aed6db101f273d8ef8689f59a
Javier Visconti <javiervisconti7@gmail.com>
2022-09-27 16:57:13 +0100

    .

--- server/src/db/index.db.js
+++ server/src/db/index.db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose= require('mongoose');
-const DB_URL=process.env.DB_URL;
+const DB_URL='mongodb+srv://javier:Z1pRHtlE92CsGqu3@cluster0.pfqrpmy.mongodb.net/note';
 
 mongoose.connect(DB_URL);
 mongoose.connection.on('error', (error)=>{
